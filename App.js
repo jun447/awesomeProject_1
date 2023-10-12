e77bb44f32e5331109ba06fe85d659d234c7440c
jun447 <junaidmumtaz16@gmail.com>
2023-10-12 18:17:29 +0500

    Added component Flatcards

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { 
+  SafeAreaView,
+        ScrollView,
         StyleSheet, 
         Text,
         View,
         useColorScheme
 } from 'react-native';
+import FlatCards from './components/FlatCards';
 
 /**
  * The main component of the app.
  * @returns {JSX.Element} The JSX element representing the app.
  */
 export default function App()  {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.container}>
-      <Text style={
-        isDarkMode ? styles.whiteText : styles.darkText
-      }>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+          <FlatCards/>
+      </ScrollView>
+    </SafeAreaView>  
   );
 }
 
