d57be2236b8b320d2b2bcfc041ba6fdc6146760a
jun447 <junaidmumtaz16@gmail.com>
2023-10-13 15:19:27 +0500

    Added Docs For Elevated Cards

--- components/ElevationCards.jsx
+++ components/ElevationCards.jsx
@@ -27,48 +27,58 @@ export default function ElevationCards() {
   )
 }
 
+/**
+ * Styles for ElevationCards component.
+ * @typedef {Object} ElevationCardsStyles
+ * @property {Object} container - Style object for the container.
+ * @property {Object} card - Style object for the card.
+ * @property {Object} cardOne - Style object for the first card.
+ * @property {Object} cardTwo - Style object for the second card.
+ * @property {Object} cardThree - Style object for the third card.
+ * @property {Object} headingText - Style object for the heading text.
+ */
 const styles = StyleSheet.create({
-    container:{
-        // flex: 1,
-        // flexDirection: 'row',
-        borderWidth: 1,
-        borderColor: 'black',
-        padding: 10,
-       
+  container:{
+    // flex: 1,
+    // flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+     
+  },
+  card:{
+    height: 100,
+    width: 100,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    card:{
-        height: 100,
-        width: 100,
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        padding: 20,
-        margin: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    cardOne:{
-          backgroundColor: 'orangered',
-    },
-    cardTwo:{
-          backgroundColor: '#3cb371',
-    },
-    cardThree:{
-          backgroundColor: '#00ced1',
-    },
-    headingText:{
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'black',
-        padding: 10,
-        borderWidth: 1,
-        borderColor: 'black',
-    }
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    padding: 20,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardOne:{
+      backgroundColor: 'orangered',
+  },
+  cardTwo:{
+      backgroundColor: '#3cb371',
+  },
+  cardThree:{
+      backgroundColor: '#00ced1',
+  },
+  headingText:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+  }
 })
